4516351be6dfcb696ef53dc8742565ac7fbc7b46
Alexey Apalenov <aapalenov@artel-workshop.com>
2023-02-22 22:50:11 +0600

    #15: fixed forgot password email template.

--- resources/views/emails/forgot_password.blade.php
+++ resources/views/emails/forgot_password.blade.php
@@ -1,58 +1,43 @@
 @extends('layouts.mail')
 
 @section('content')
-    <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
-           style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
-        <!-- Body content -->
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
         <tr>
-            <td class="content-cell"
-                style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">
-                    Hello!</h1>
-                <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                    You are receiving this email because we received a password reset request for your account.</p>
-                <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0"
-                       style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; padding: 0; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
+            <td align="left"
+                style="padding: 0 0 20px 0; font-size: 16px; line-height: 25px; font-family: 'Atkinson Hyperlegible', sans-serif; color: #c1c1c0;"
+                class="padding-copy"
+            >
+                You have requested to reset your password.
+            </td>
+        </tr>
+
+        <tr>
+            <td align="left"
+                style="padding: 0 0 20px 0; font-size: 16px; line-height: 25px; font-family: 'Atkinson Hyperlegible', sans-serif; color: #c1c1c0;"
+                class="padding-copy"
+            >
+                Please click the button to reset your password.
+            </td>
+        </tr>
+
+        <tr>
+            <td align="center">
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
                     <tr>
-                        <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                            <table width="100%" border="0" cellpadding="0" cellspacing="0"
-                                   style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
+                        <td align="center" style="padding-top: 25px;" class="padding">
+                            <table border="0" cellspacing="0" cellpadding="0" class="mobile-button-container">
                                 <tr>
-                                    <td align="center"
-                                        style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                        <table border="0" cellpadding="0" cellspacing="0"
-                                               style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                            <tr>
-                                                <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                                    <a href="{{config('app.frontend_url')}}/enter-new-password/{{$hash}}"
-                                                       class="button button-blue" target="_blank"
-                                                       style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3097D1; border-top: 10px solid #3097D1; border-right: 18px solid #3097D1; border-bottom: 10px solid #3097D1; border-left: 18px solid #3097D1;">Reset
-                                                        Password</a>
-                                                </td>
-                                            </tr>
-                                        </table>
+                                    <td align="center" style="border-radius: 10px;" bgcolor="#c1c1c0">
+                                        <a
+                                                href="{{config('app.frontend_url')}}/enter-new-password?token={{$hash}}"
+                                                target="_blank"
+                                                style="border-radius: 10px; font-family: Prozak, sans-serif; font-weight: 700; font-size: 2em; letter-spacing: 4px; color: #3a3a39; text-decoration: none; padding: 5px 25px; border: 1px solid #c1c1c0; display: inline-block;"
+                                                class="mobile-button">
+                                            RESET PASSWORD
+                                        </a>
                                     </td>
                                 </tr>
                             </table>
-                        </td>
-                    </tr>
-                </table>
-                <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                    If you did not request a password reset, no further action is required.</p>
-                <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                    Regards,<br>{{config('app.name')}}</p>
-                <table class="subcopy" width="100%" cellpadding="0" cellspacing="0"
-                       style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-top: 1px solid #EDEFF2; margin-top: 25px; padding-top: 25px;">
-                    <tr>
-                        <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                            <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787E; line-height: 1.5em; margin-top: 0; text-align: left; font-size: 12px;">
-                                If you’re having trouble clicking the "Reset Password" button, copy and paste the URL
-                                below into your web browser:
-                                <a href="{{config('app.frontend_url')}}/enter-new-password/{{$hash}}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;"></a>
-                                <a href="{{config('app.frontend_url')}}/enter-new-password/{{$hash}}" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #3869D4;">
-                                    {{config('app.frontend_url')}}/enter-new-password/{{$hash}}
-                                </a>
-                            </p>
                         </td>
                     </tr>
                 </table>
