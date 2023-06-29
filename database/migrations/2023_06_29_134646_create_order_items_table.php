b6cd3d28f2f5b2100decb572b7c2a83ac18e600f
vkokhan <kvolodymyr.dev@gmail.com>
2023-06-29 16:49:48 +0300

    feat order_items table

---
+++ database/migrations/2023_06_29_134646_create_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('item_id')->constrained('products');
+            $table->integer('item_quantity');
+            $table->integer('price');
+            $table->integer('vendor_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+};
