5d3c813d5049a856fbee5357a1b9dd15c30f41be
vkokhan <kvolodymyr.dev@gmail.com>
2023-07-03 09:49:23 +0300

    fix invoice

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -55,7 +55,7 @@ class UserService extends EntityService
     }
 
     public function update($where, $data)
-    {   dd($data);
+    {  
 
         if (!empty($data['password'])) {
             $data['password'] = Hash::make($data['password']);
